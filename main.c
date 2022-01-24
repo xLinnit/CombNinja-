c10913a363f19ab3bbeb45c5ef32840827978356
FoxiDev <shafeqsous1234@gmail.com>
2022-01-24 22:19:59 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,10 +2,13 @@
 #include <string.h>
 
 #define OPTIONS 3
+#define HEX 16
 
 const char options_label[OPTIONS][18] = {"Domain Classifier", "Combs Joiner", "Hash Fixer"};
 void operations_viewer();
 void logo();
+
+void hash_fixer(char *comb, int fdes);
 
 int main(int argc, char const *argv[]) {
     int quit = 0;
@@ -39,4 +42,41 @@ void operations_viewer()  {
   for (loop_index = 0; loop_index < OPTIONS; ++loop_index) {
     printf("%d| %s\n", loop_index + 1, options_label[loop_index]);
   }
+}
+
+void hash_fixer(char *comb, int fdes)   {
+    int r = 0;
+    char fixed_comb[256];
+    char digits[2];
+    digits[0] = 47;
+    digits[1] = 47;
+
+    int loop_index_i = 0, loop_index_j = 0;
+
+    for (loop_index_i = 0; loop_index_i < HEX; ++loop_index_i)  {
+        if(loop_index_i < 10)   {
+            digits[0]++;
+        }else if(loop_index_i == 10)    {
+            digits[0] = 48;
+            digits[0] = digits[0] + 49;
+        }else if(loop_index_i > 10) {
+            digits[0]++;
+        }
+        for(loop_index_j = 0; loop_index_j < HEX; ++loop_index_j)   {
+            if(loop_index_j < 10)   {
+                digits[1]++;
+            }else if(loop_index_j == 10)    {
+                digits[1] = 48;
+                digits[1] = digits[1] + 49;
+            }else if(loop_index_j > 10) {
+                digits[1]++;
+            }
+            sprintf(fixed_comb, "%s%c%c%c", comb, digits[0], digits[1], '\n');
+            r = write(fdes, fixed_comb, strlen(fixed_comb));
+        }
+        digits[1] = 47;
+    }
+    printf("[hash_fixer()] The hash of [%s] has been fixed\n", comb);
+    strcpy(fixed_comb, "");
+    r = 0;
 }
